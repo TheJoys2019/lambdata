98a38862f7eac726072ca448a4b591a9049a7a32
TheJoys2019 <artin.sinani@gmail.com>
2019-05-30 12:29:22 -0700

    Modified with helper functions

--- lambdata_TheJoys2019/__init__.py
+++ lambdata_TheJoys2019/__init__.py
@@ -7,6 +7,8 @@ lambdata - a collection of Data Science helper functions
 import pandas as pd
 import numpy as np
 import example_module
+import null_doctor
+import datetime_doctor
 
 Y = example_module.increment(example_module.x)
 TEST = pd.DataFrame(np.ones(10))
